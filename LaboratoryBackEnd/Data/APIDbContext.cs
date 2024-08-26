468600d3a2b3ab1ef64470f8f1c4da67a653999b
alex_kaam <contato@asksystems.com.br>
2024-08-26 19:22:41 -0300

    -Initial commit

---
+++ LaboratoryBackEnd/Data/APIDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LaboratoryBackEnd.Data
+{
+    public class APIDbContext : DbContext
+    {
+        //public DbSet<Cliente> Clientes { get; set; }
+
+        public APIDbContext(DbContextOptions<APIDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
---
+++ LaboratoryBackEnd/Data/Interface/IRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace LaboratoryBackEnd.Data.Interface
+{
+    public interface IRepository<T>
+    {
+        Task<IEnumerable<T>> GetItems();
+        Task<T> GetItem(int id);
+        Task<IEnumerable<T>> GenericQuery(IQueryable<T> query);
+        Task Put(T imovel);
+        Task<T> Post(T item);
+        Task Delete(int id);
+        bool Exists(int id);
+        void RemoveContex(T item);
+        IQueryable<T> Query();
+        int GetLasdOrOne();
+    }
+}
