74cfbb227d3dc3b49fa9052d84d25e5c7c563059
alex_kaam <contato@asksystems.com.br>
2024-09-25 16:03:08 -0300

    + Fields Client

--- LaboratoryBackEnd/Models/Cliente.cs
+++ LaboratoryBackEnd/Models/Cliente.cs
@@ -3,42 +3,86 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-    [Table("clientes")]
-    public class Cliente : IIdentifiable
-    {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [Column("cliente_id")]
-        public int ID { get; set; }
+[Table("clientes")]
+public class Cliente : IIdentifiable
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("cliente_id")]
+    public int ID { get; set; }
 
-        [Required]
-        [StringLength(255)]
-        [Column("nome")]
-        public string Nome { get; set; }
+    [Required]
+    [StringLength(255)]
+    [Column("nome")]
+    public string Nome { get; set; }
 
-        [StringLength(20)]
-        [Column("cpf_cnpj")]
-        public string CpfCnpj { get; set; }
+    [StringLength(20)]
+    [Column("cpf_cnpj")]
+    public string CpfCnpj { get; set; }
 
-        [StringLength(255)]
-        [Column("endereco")]
-        public string Endereco { get; set; }
+    [StringLength(255)]
+    [Column("endereco")]
+    public string Endereco { get; set; }
 
-        [StringLength(20)]
-        [Column("telefone")]
-        public string Telefone { get; set; }
+    [StringLength(20)]
+    [Column("telefone")]
+    public string Telefone { get; set; }
 
-        [Required]
-        [StringLength(100)]
-        [Column("email")]
-        public string Email { get; set; }
+    [Required]
+    [StringLength(100)]
+    [Column("email")]
+    public string Email { get; set; }
 
-        [Required]
-        [Column("situacao_id")]
-        public int SituacaoId { get; set; }
+    [Required]
+    [Column("situacao_id")]
+    public int SituacaoId { get; set; }
 
-        [Required]
-        [Column("data_cadastro")]
-        public DateTime DataCadastro { get; set; }
-    }
+    [Required]
+    [Column("data_cadastro")]
+    public DateTime DataCadastro { get; set; }
+
+    // Novos campos adicionados de acordo com a tabela
+    [StringLength(1)]
+    [Column("sexo")]
+    public string Sexo { get; set; }
+
+    [Column("nascimento")]
+    public DateTime? Nascimento { get; set; }
+
+    [Required]
+    [Column("convenio_id")]
+    public int ConvenioId { get; set; }
+
+    [Required]
+    [Column("plano_id")]
+    public int PlanoId { get; set; }
+
+    [StringLength(20)]
+    [Column("rg")]
+    public string RG { get; set; }
+
+    [StringLength(255)]
+    [Column("razao_social")]
+    public string RazaoSocial { get; set; }
+
+    [StringLength(20)]
+    [Column("ie")]
+    public string IE { get; set; }
+
+    [StringLength(20)]
+    [Column("im")]
+    public string IM { get; set; }
+
+    [StringLength(255)]
+    [Column("nome_responsavel")]
+    public string NomeResponsavel { get; set; }
+
+    [StringLength(20)]
+    [Column("cpf_responsavel")]
+    public string CpfResponsavel { get; set; }
+
+    [StringLength(20)]
+    [Column("telefone_responsavel")]
+    public string TelefoneResponsavel { get; set; }
+}
 
