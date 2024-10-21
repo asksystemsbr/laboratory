3483f68da290f660be10dfe6ed36843ba73df141
João Paulo <jp17paulo@terra.com.br>
2024-10-21 20:55:11 -0300

    feat: Ajustes relacionados a lógica de inclusão/exclusão de convênios/planos

--- LaboratoryBackEnd/Controllers/RecepcaoConvenioPlanoController.cs
+++ LaboratoryBackEnd/Controllers/RecepcaoConvenioPlanoController.cs
@@ -97,20 +97,10 @@ namespace LaboratoryBackEnd.Controllers
             return NoContent();
         }
 
-        /// <summary>
-        /// Adiciona ou atualiza os convênios e planos para uma recepção específica
-        /// </summary>
-        /// <param name="recepcaoId">ID da recepção</param>
-        /// <param name="conveniosPlanos">Lista de convênios e planos a serem adicionados ou atualizados</param>
+
         /// <returns>Nenhum conteúdo se a operação for bem-sucedida</returns>
         [HttpPost("addOrUpdate/{recepcaoId}")]
         //[Authorize(Policy = "CanWrite")]
-        [SwaggerOperation(Summary = "Adiciona ou atualiza convênios e planos para uma recepção",
-                          Description = "Este endpoint adiciona novos convênios e planos ou atualiza os existentes para uma recepção específica. Também atualiza o campo de restrição.")]
-        [SwaggerResponse((int)HttpStatusCode.NoContent, "Operação realizada com sucesso")]
-        [SwaggerResponse((int)HttpStatusCode.BadRequest, "Requisição inválida")]
-        [SwaggerResponse((int)HttpStatusCode.Unauthorized, "Não autorizado")]
-        [SwaggerResponse((int)HttpStatusCode.InternalServerError, "Erro interno do servidor")]
         public async Task<IActionResult> AddOrUpdateRecepcaoConvenioPlanos(
             [SwaggerParameter("ID da recepção", Required = true)] int recepcaoId,
             [SwaggerParameter("Lista de convênios e planos", Required = true)][FromBody] List<RecepcaoConvenioPlano> conveniosPlanos)
@@ -122,8 +112,6 @@ namespace LaboratoryBackEnd.Controllers
             }
             catch (Exception ex)
             {
-                // Log the exception
-                //_loggerService.LogError(ex, $"Erro ao adicionar/atualizar convênios e planos para recepção {recepcaoId}");
                 return StatusCode(500, "Ocorreu um erro interno ao processar sua solicitação.");
             }
         }
